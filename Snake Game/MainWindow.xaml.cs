5d3f7712ba2868ee4a3e0da3e250b08f1ebcc2ca
mitaka621 <d418201@gmail.com>
2023-03-10 21:36:11 +0200

    UI Added

--- Snake Game/MainWindow.xaml.cs
+++ Snake Game/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Snake_Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,28 @@ namespace Snake_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int rows = 15, cols = 15;
+        private Image[,] gridImages;
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetUpGrid();
+            gridImages[1,2].Source=gridVal
+        }
+        private Image[,] SetUpGrid()
+        {
+            Image[,] images=new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    images[i, j] = new Image() { Source = Images.Empty };
+                    GameGrid.Children.Add(images[i, j]);
+                }
+            }
+            return images;
         }
     }
 }
---
+++ Snake Game/Models/Images.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake_Game.Models
+{
+    public static class Images
+    {
+        public static readonly ImageSource Empty = LoadImage("Empty.png");
+        public static readonly ImageSource Body = LoadImage("Body.png");
+        public static readonly ImageSource Head = LoadImage("Head.png");
+        public static readonly ImageSource Food = LoadImage("Food.png");
+        public static readonly ImageSource DeadBody = LoadImage("DeadBody.png");
+        public static readonly ImageSource DeadHead = LoadImage("DeadHead.png");
+        private static ImageSource LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri($"Assets/{filename}", UriKind.Relative));
+        }
+    }
+}
